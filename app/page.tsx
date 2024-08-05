708b40c0a6bf6d65f203c107d9152c81c55bf0b3
안동훈 Ahn Donghoon <switzch3@gmail.com>
2024-08-05 11:28:59 +0900

    오픈그래프정보 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
 import { Metadata } from "next";
+import Head from "next/head";
 import Main from "@/app/Main";
 
-export const metadata: Metadata = {
-  title: "RollEase - Finding wheelchair-accessible hospitals",
-};
+// export const metadata: Metadata = {
+//   title: "RollEase - Finding wheelchair-accessible hospitals",
+// };
 export default function Home() {
-  return <Main />;
+  return (
+    <>
+      <Head>
+        <title>RollEase - Finding wheelchair-accessible hospitals</title>
+
+        <meta name="description" content="휠체어 접근이 가능한 병원찾기" />
+
+        <meta property="og:url" content="https://roll-ease.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="RollEase - Finding wheelchair-accessible hospitals"
+        />
+        <meta
+          property="og:description"
+          content="휠체어 접근이 가능한 병원찾기"
+        />
+        <meta property="og:image" content="/logo.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="roll-ease.vercel.app" />
+        <meta property="twitter:url" content="https://roll-ease.vercel.app/" />
+        <meta
+          name="twitter:title"
+          content="RollEase - Finding wheelchair-accessible hospitals"
+        />
+        <meta
+          name="twitter:description"
+          content="휠체어 접근이 가능한 병원찾기"
+        />
+        <meta name="twitter:image" content="/logo.png" />
+      </Head>
+      <Main />
+    </>
+  );
 }
