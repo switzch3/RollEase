442ab3acee23f1763d539995fccc0ab115d2000b
Ahn Dong-hoon <switzch3@gmail.com>
2025-06-29 13:14:15 +0900

    플젝 이름 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,16 +5,17 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RollEase - Finding wheelchair-accessible hospitals",
-  description: "휠체어 접근이 가능한 병원찾기",
+  // title: "RollEase - Finding wheelchair-accessible hospitals",
+  title: "EC 2025 Summer",
+  // description: "휠체어 접근이 가능한 병원찾기",
   openGraph: {
-    title: "RollEase - Finding wheelchair-accessible hospitals",
-    description: "휠체어 접근이 가능한 병원찾기",
-    url: "https://roll-ease.vercel.app",
+    title: "EC 2025 Summer",
+    // description: "휠체어 접근이 가능한 병원찾기",
+    url: "https://ec2025-nine.vercel.app",
     siteName: "RollEase",
     images: [
       {
-        url: "https://roll-ease.vercel.app/og.png", // Must be an absolute URL
+        url: "https://ec2025-nine.vercel.app/og.png", // Must be an absolute URL
         width: 512,
         height: 246,
       },
