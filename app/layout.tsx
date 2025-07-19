78c1cb9e50a89ac7d1cc6df27c2c0aa64fc7e013
Ahn Dong-hoon <switzch3@gmail.com>
2025-07-19 13:02:39 +0900

    로고파일 및 앱이름 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,16 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  // title: "RollEase - Finding wheelchair-accessible hospitals",
-  title: "EC 2025 Summer",
-  // description: "휠체어 접근이 가능한 병원찾기",
+  title: "WheelWay - Finding wheelchair-accessible hospitals",
+  description: "휠체어 접근이 가능한 병원찾기",
   openGraph: {
-    title: "EC 2025 Summer",
-    // description: "휠체어 접근이 가능한 병원찾기",
-    url: "https://ec2025-nine.vercel.app",
-    siteName: "RollEase",
+    title: "WheelWay - Finding wheelchair-accessible hospitals",
+    description: "휠체어 접근이 가능한 병원찾기",
+    url: "https://wheelway.vercel.app",
+    siteName: "WheelWay",
     images: [
       {
-        url: "https://ec2025-nine.vercel.app/og.png", // Must be an absolute URL
+        url: "https://wheelway.vercel.app/og.png", // Must be an absolute URL
         width: 512,
         height: 246,
       },
